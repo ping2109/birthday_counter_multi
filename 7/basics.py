d3090eb24b482504be4f6b8ade2210cef8e8fbdd
pingmado <75196272+ping2109@users.noreply.github.com>
2021-10-12 13:39:54 +0700

    Add files via upload

---
+++ 7/basics.py
@@ -0,0 +1,52 @@
+import os
+import time
+import datetime
+import telegram
+from telegram import Sticker, PhotoSize, TelegramError, StickerSet, MaskPosition, Bot
+from telegram.error import BadRequest
+
+bot_token = os.getenv('TOKEN')
+channel = os.getenv('TG_ID')
+
+username = os.getenv('USERNAME7')
+
+def get_user_birthday():
+    year = int(os.getenv('YEAR7'))
+    month = int(os.getenv('MONTH7'))
+    day = int(os.getenv('DATE7')) + 1
+    birthday = datetime.datetime(year, month, day)
+    return birthday
+
+def compute_birthday_difference(original_date, now):
+    date1 = now
+    date2 = datetime.datetime(now.year, original_date.month, original_date.day)
+    difference = date1 - date2
+    days = int(difference.total_seconds()/60/60/24)
+    return days
+
+def main():
+    bday = get_user_birthday()
+    now = datetime.datetime.now()
+    days = compute_birthday_difference(bday, now)
+    days2 = int(365)
+    if days < 0:
+        telegram_notify = telegram.Bot(bot_token)
+        message = (f"""{username}'s birthday is in {format(-days)} days.""")
+
+        telegram_notify.send_message(chat_id=channel, text=message, disable_web_page_preview=True,
+                                parse_mode='Markdown')
+    elif days > 0:
+        telegram_notify = telegram.Bot(bot_token)
+        message = (f"""{username}'s birthday is in {days2 - days} days.""")
+
+        telegram_notify.send_message(chat_id=channel, text=message, disable_web_page_preview=True,
+                                parse_mode='Markdown')
+    else:
+        telegram_notify = telegram.Bot(bot_token)
+        message = (f"""Happy Birthday to {username}!""")
+
+        telegram_notify.send_message(chat_id=channel, text=message, disable_web_page_preview=True,
+                                parse_mode='Markdown')
+
+        
+main()
